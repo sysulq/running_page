96c2728d88c4752c6bd2a560fcc747b17526cec1
大可 <hnlq.sysu@gmail.com>
2025-07-09 13:40:13 +0800

    fix: remove outdated navigation links from site metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,7 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://sysulq.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
@@ -25,12 +25,8 @@ const data: ISiteMetadataResult = {
       url: `${getBasePath()}/summary`,
     },
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'Github',
+      url: 'https://github.com/sysulq',
     },
   ],
 };
